5ef0878b67f58abcdcd3b8ac960a4307ceeaf0d0
v1xtron <89225495+Vulnpire@users.noreply.github.com>
2024-11-01 09:17:19 +0100

    Update init.sh

--- scripts/web/basic/init.sh
+++ scripts/web/basic/init.sh
@@ -1,10 +1,6 @@
 # script.sh
 #!/bin/bash
 
-current_path=$(pwd)
-fourth_component=$(echo $current_path | cut -d'/' -f5) # 4 for root, 5 for -a
-NOTIFY_ID=$fourth_component
-
 while true; do
     source "./script.sh"
 
