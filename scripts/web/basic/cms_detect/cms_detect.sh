ecafdecea3a4f4c1b308cdf9e0221f8227170852
v1xtron <89225495+Vulnpire@users.noreply.github.com>
2024-11-29 14:42:02 +0100

    Update cms_detect.sh

--- scripts/web/basic/cms_detect/cms_detect.sh
+++ scripts/web/basic/cms_detect/cms_detect.sh
@@ -23,9 +23,21 @@ declare -A cms_patterns=(
   ["government"]="govcms|openpublic|civicspace"
   ["enterprise"]="sitecore|kentico|episerver|dotcms|opentext|adobe experience manager|aem"
   ["servers"]="iis|apache|nginx|tomcat|gunicorn|uwsgi|express|lighttpd|jetty|centos"
-  ["titles"]="powered by|index of|welcome to|hosted by|proudly served by|dashboard"
+  ["admin_panels"]="admin|administrator|controlpanel|cpanel|backend|login|auth|secure|support|helpdesk|manage|dashboard|signin|admin dashboard|user/login|admin/login"
+  ["sensitive_subdomains"]="staging|test|dev|qa|beta|uat|internal|secure|private|backup|old|demo|sandbox|preprod|mail|email|smtp|imap|pop3|static|debug|logs"
+  ["open_directories"]="index of|directory listing|file listing|parent directory|dir browse|directory contents|open directory|file explorer|browsable directory"
+  ["api_endpoints"]="api|rest api|graphql|swagger|openapi|postman|http api|json|xml|soap|api docs"
+  ["clouds"]="aws|amazon s3|google cloud|gcp|heroku|digitalocean|linode|vultr|rackspace|bucket"
+  ["dashboard_setup"]="setup|wizard|install|installation|onboarding|config|configuration|setup wizard|first time setup|setup dashboard|init|initialization|setup/login"
+  ["titles"]="powered by|index of|welcome to|hosted by|proudly served by|dashboard|login page|home page|sign in|error page|403 forbidden|404 not found|500 internal server error|access denied|service unavailable"
+  ["frameworks"]="django|flask|spring boot|rails|asp.net|laravel|codeigniter|express|fastapi|symfony|nestjs|react|angular|vue|svelte"
+  ["databases"]="mysql|mariadb|postgresql|mongo|redis|sqlite|oracle|mssql|couchdb|dynamodb"
+  ["technologies"]="nginx|apache|iis|tomcat|gunicorn|uwsgi|lighttpd|nodejs|express|centos|ubuntu|debian|kubernetes|docker|helm|traefik|haproxy"
+  ["email"]="smtp|imap|pop3|mailgun|sendgrid|postfix|exim|zimbra|exchange|roundcube|webmail"
+  ["monitoring"]="prometheus|grafana|zabbix|splunk|datadog|new relic|appdynamics|kibana|elk stack|fluentd|graylog"
+  ["wiki"]="mediawiki|tiki wiki|twiki|xwiki|dokuwiki"
 )
 
 for category in "${!cms_patterns[@]}"; do
-  echo "$input" | grep -Ei "${cms_patterns[$category]}" | notify -id "$category" -bulk -d 2 -silent
+  echo "$input" | grep -Ei "${cms_patterns[$category]}" | notify -id "$category" -bulk -d 4 -silent
 done
