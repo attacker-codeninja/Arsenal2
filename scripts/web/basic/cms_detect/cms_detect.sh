998e53774f698647774650d35ff9bbb2e6a264b7
v1xtron <89225495+Vulnpire@users.noreply.github.com>
2024-11-30 08:48:07 +0100

    improved regex

--- scripts/web/basic/cms_detect/cms_detect.sh
+++ scripts/web/basic/cms_detect/cms_detect.sh
@@ -8,25 +8,25 @@ fi
 input=$(cat)
 
 declare -A cms_patterns=(
-  ["wordpress"]="wordpress|wp|wp engine|powered by wordpress"
-  ["jira"]="jira|confluence|atlassian|bitbucket|bamboo"
-  ["drupal"]="drupal|drupal cms"
-  ["joomla"]="joomla|joomla cms|joomla version"
+  ["wordpress"]="\\bwordpress\\b|\\bwp\\b|wp engine|powered by wordpress"
+  ["jira"]="\\bjira\\b|\\bconfluence\\b|\\batlassian\\b|bitbucket|bamboo"
+  ["drupal"]="\\bdrupal\\b|drupal cms"
+  ["joomla"]="\\bjoomla\\b|joomla cms|joomla version"
   ["ecommerce"]="magento|shopify|prestashop|opencart|bigcommerce|woocommerce|nopcommerce|saleor|sylius|miva|vtiger"
   ["general"]="typo3|umbraco|dotnetnuke|plone|ghost|strapi|orchard|silverstripe|textpattern|weebly|squarespace|duda|sitefinity|cmsmadesimple|serendipity|aem|kentico|openpublic|civicspace"
   ["forums"]="xenforo|vbulletin|phpbb|discourse|vanilla forums|mybb|fudforum|simple machines forum|fluxbb"
-  ["education"]="moodle|blackboard|sakai|canvas lms|dokeos|claroline"
+  ["education"]="\\bmoodle\\b|blackboard|sakai|canvas lms|dokeos|claroline"
   ["portal"]="liferay|wix|jimdo|zoho sites|webflow|webnode|djangocms|mezzanine|pyrocms|bolt cms|statamic|bludit|episerver"
   ["php"]="phpmyadmin|cakephp|codeigniter|fuelphp|zend framework|laravel|yii framework|symfony"
   ["wiki"]="mediawiki|tiki wiki|twiki|xwiki|dokuwiki"
   ["static"]="jekyll|hugo|gatsby|netlify cms|pelican"
   ["government"]="govcms|openpublic|civicspace"
   ["enterprise"]="sitecore|kentico|episerver|dotcms|opentext|adobe experience manager|aem"
-  ["servers"]="iis|apache|nginx|tomcat|gunicorn|uwsgi|express|lighttpd|jetty|centos"
-  ["admin_panels"]="admin|administrator|controlpanel|cpanel|backend|login|auth|secure|support|helpdesk|manage|dashboard|signin|admin dashboard|user/login|admin/login"
-  ["sensitive_subdomains"]="staging|test|dev|qa|beta|uat|internal|secure|private|backup|old|demo|sandbox|preprod|mail|email|smtp|imap|pop3|static|debug|logs"
+  ["servers"]="\\biis\\b|apache|nginx|tomcat|gunicorn|uwsgi|express|lighttpd|jetty|centos"
+  ["admin_panels"]="\\b(admin|administrator|controlpanel|cpanel|backend|login|auth|secure|support|helpdesk|manage|dashboard|signin|admin dashboard|user/login|admin/login)\\b"
+  ["sensitive_subdomains"]="\\b(staging|test|dev|qa|beta|uat|internal|secure|private|backup|old|demo|sandbox|preprod|mail|email|smtp|imap|pop3|debug|logs|monitoring|admin|api|vpn|portal|config|setup|install|setup-login|prototype|hidden|temp|temp-login|identity|core|auth|auth-server|docs|storage|billing|payments|invoice|files|upload|cdn|assets|img|analytics|stats|gateway)\\b"
   ["open_directories"]="index of|directory listing|file listing|parent directory|dir browse|directory contents|open directory|file explorer|browsable directory"
-  ["api_endpoints"]="api|rest api|graphql|swagger|openapi|postman|http api|json|xml|soap|api docs"
+  ["api_endpoints"]="\\b(api|rest api|graphql|swagger|openapi|json|xml|soap|api docs)\\b"
   ["clouds"]="aws|amazon s3|google cloud|gcp|heroku|digitalocean|linode|vultr|rackspace|bucket"
   ["dashboard_setup"]="setup|wizard|install|installation|onboarding|config|configuration|setup wizard|first time setup|setup dashboard|init|initialization|setup/login"
   ["titles"]="powered by|index of|welcome to|hosted by|proudly served by|dashboard|login page|home page|sign in|error page|403 forbidden|404 not found|500 internal server error|access denied|service unavailable"
@@ -35,9 +35,11 @@ declare -A cms_patterns=(
   ["technologies"]="nginx|apache|iis|tomcat|gunicorn|uwsgi|lighttpd|nodejs|express|centos|ubuntu|debian|kubernetes|docker|helm|traefik|haproxy"
   ["email"]="smtp|imap|pop3|mailgun|sendgrid|postfix|exim|zimbra|exchange|roundcube|webmail"
   ["monitoring"]="prometheus|grafana|zabbix|splunk|datadog|new relic|appdynamics|kibana|elk stack|fluentd|graylog"
-  ["wiki"]="mediawiki|tiki wiki|twiki|xwiki|dokuwiki"
 )
 
 for category in "${!cms_patterns[@]}"; do
-  echo "$input" | grep -Ei "${cms_patterns[$category]}" | notify -id "$category" -bulk -d 4 -silent
+  matches=$(echo "$input" | grep -Ei "${cms_patterns[$category]}")
+  if [ -n "$matches" ]; then
+    echo "$matches" | notify -id "$category" -bulk -d 4 -silent
+  fi
 done
